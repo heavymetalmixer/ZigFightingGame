99003c887136739028539409a90963cb9b694d47
heavymetalmixer <heavymetalmixer@gmail.com>
2025-01-13 13:28:34 -0500

    Linked Raylib-Zig as a dependency and created a window with Raylib functions

--- src/main.zig
+++ src/main.zig
@@ -1,27 +1,64 @@
+// const std = @import("std");
+// const pt: type = @import("pTest");
+
+// pub fn main() !void {
+//     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
+//     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+
+//     // stdout is for the actual output of your application, for example if you
+//     // are implementing gzip, then only the compressed bytes should be sent to
+//     // stdout, not any debugging messages.
+//     const stdout_file = std.io.getStdOut().writer();
+//     var bw = std.io.bufferedWriter(stdout_file);
+//     const stdout = bw.writer();
+
+//     try stdout.print("Run `zig build test` to run the tests.\n", .{});
+
+//     try bw.flush(); // don't forget to flush!
+
+//     pt.printTest();
+
+//     const a: u128 = 10;
+//     std.debug.print("Variable a {}\n\n",.{a});
+// }
+
+// test "simple test" {
+//     var list = std.ArrayList(i32).init(std.testing.allocator);
+//     defer list.deinit(); //try commenting this out and see if zig detects the memory leak!
+//     try list.append(42);
+//     try std.testing.expectEqual(@as(i32, 42), list.pop());
+// }
+
 const std = @import("std");
+const rl = @import("raylib");
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+pub fn main() anyerror!void {
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const screenWidth = 800;
+    const screenHeight = 450;
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    rl.initWindow(screenWidth, screenHeight, "raylib-zig [core] example - basic window");
+    defer rl.closeWindow(); // Close window and OpenGL context
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    rl.setTargetFPS(60); // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
 
-    try bw.flush(); // don't forget to flush!
+    // Main game loop
+    while (!rl.windowShouldClose()) { // Detect window close button or ESC key
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
 
-    const a: u128 = 10;
-    std.debug.print("Variable a {}",.{a});
-}
+        // Draw
+        //----------------------------------------------------------------------------------
+        rl.beginDrawing();
+        defer rl.endDrawing();
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); //try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+        rl.clearBackground(rl.Color.white);
+
+        rl.drawText("Congrats! You created your first window!", 190, 200, 20, rl.Color.brown);
+        //----------------------------------------------------------------------------------
+    }
 }
