78c64d8fcea155ceb77b0cfb419e43778b984775
heavymetalmixer <heavymetalmixer@gmail.com>
2025-01-15 11:18:48 -0500

    Added raylib-zig-devel as a git submodule inside the libs folder

--- build.zig
+++ build.zig
@@ -104,7 +104,6 @@
 
 
 const std = @import("std");
-const rlz = @import("raylib-zig");
 
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
@@ -116,33 +115,19 @@ pub fn build(b: *std.Build) !void {
     });
 
     const raylib = raylib_dep.module("raylib");
+    const raygui = raylib_dep.module("raygui");
     const raylib_artifact = raylib_dep.artifact("raylib");
 
-    //web exports are completely separate
-    if (target.query.os_tag == .emscripten) {
-        const exe_lib = try rlz.emcc.compileForEmscripten(b, "ZigFightingGame", "src/main.zig", target, optimize);
-
-        exe_lib.linkLibrary(raylib_artifact);
-        exe_lib.root_module.addImport("raylib", raylib);
-
-        // Note that raylib itself is not actually added to the exe_lib output file, so it also needs to be linked with emscripten.
-        const link_step = try rlz.emcc.linkWithEmscripten(b, &[_]*std.Build.Step.Compile{ exe_lib, raylib_artifact });
-        //this lets your program access files like "resources/my-image.png":
-        link_step.addArg("--embed-file");
-        link_step.addArg("resources/");
-
-        b.getInstallStep().dependOn(&link_step.step);
-        const run_step = try rlz.emcc.emscriptenRunStep(b);
-        run_step.step.dependOn(&link_step.step);
-        const run_option = b.step("run", "Run ZigFightingGame");
-        run_option.dependOn(&run_step.step);
-        return;
-    }
-
-    const exe = b.addExecutable(.{ .name = "ZigFightingGame", .root_source_file = b.path("src/main.zig"), .optimize = optimize, .target = target });
+    const exe = b.addExecutable(.{
+        .name = "ZigFightingGame",
+        .root_source_file = b.path("src/main.zig"),
+        .optimize = optimize,
+        .target = target
+    });
 
     exe.linkLibrary(raylib_artifact);
     exe.root_module.addImport("raylib", raylib);
+    exe.root_module.addImport("raygui", raygui);
 
     const run_cmd = b.addRunArtifact(exe);
     const run_step = b.step("run", "Run ZigFightingGame");
